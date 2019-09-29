5ec65951af43aafeb0ba82a5138510bf6433b61a
Adrian Smijulj <adrian1358@gmail.com>
2019-09-29 09:50:36 +0200

    chore: update jest config

--- scripts/jest/config.dist.js
+++ scripts/jest/config.dist.js
@@ -3,8 +3,8 @@ const baseConfig = require("./config.base");
 // Create a module map to point packages to the build output
 const moduleNameMapper = {};
 
-moduleNameMapper["^all-deps-in-order/(.*)$"] = "<rootDir>dist/$1";
-moduleNameMapper["^all-deps-in-order$"] = "<rootDir>dist";
+moduleNameMapper["^adio/(.*)$"] = "<rootDir>dist/$1";
+moduleNameMapper["^adio$"] = "<rootDir>dist";
 
 module.exports = Object.assign({}, baseConfig, {
     moduleNameMapper
--- scripts/jest/config.src.js
+++ scripts/jest/config.src.js
@@ -3,8 +3,8 @@ const baseConfig = require("./config.base");
 // Create a module map to point packages to the build output
 const moduleNameMapper = {};
 
-moduleNameMapper["^all-deps-in-order/(.*)$"] = "<rootDir>src/$1";
-moduleNameMapper["^all-deps-in-order$"] = "<rootDir>src";
+moduleNameMapper["^adio/(.*)$"] = "<rootDir>src/$1";
+moduleNameMapper["^adio$"] = "<rootDir>src";
 
 module.exports = Object.assign({}, baseConfig, {
     moduleNameMapper,
