ceae7baf7c6b1901b61117a457793848544c096c
Adrian Smijulj <adrian1358@gmail.com>
2019-10-13 16:31:52 +0200

    feat: show errors output via console

--- src/bin/adio.js
+++ src/bin/adio.js
@@ -1,80 +1,52 @@
 #! /usr/bin/env node
 const { argv } = require("yargs");
 const chalk = require("chalk");
-const Adio = require("./../Adio");
+const Adio = require("./..");
+const cosmiconfig = require("cosmiconfig");
+const explorer = cosmiconfig("adio");
+const rootConfig = explorer.searchSync(process.cwd()) || {};
 
-const adio = new Adio({
-    packageJson: argv._,
-    ignoreDirs: argv.ignoreDirs,
-    ignoreNodeModules: argv.ignoreNodeModules
-});
+const adio = new Adio({ ...rootConfig.config, ...argv });
 
 const results = adio.test();
 
-console.log("-----------------------------------------------------------------");
-console.log(JSON.stringify(results, null, 2));
-/*
-const config = {
-    dirs: [],
-    ignore: {
-        src: [],
-        dependencies: [],
-        devDependencies: [],
-        peerDependencies: []
-    }
-};
-
-if (argv.config) {
-    Object.assign(config, require(process.cwd() + "/" + argv.config));
+const packagesWithErrors = results.filter(r => r.errors.count);
+if (packagesWithErrors.length === 0) {
+    return process.exit(0);
 }
 
-if (argv._.length) {
-    config.dirs = argv._;
-}
+packagesWithErrors.forEach((pckg, index) => {
+    console.log(chalk.red(`${index + 1}. ${pckg.packageJson.name} (${pckg.dir})`));
 
-const checks = config.dirs.map(dir => adio.test(adio));
-
-Promise.all(checks).then(results => {
-    const packagesWithErrors = results.filter(r => r.errors.count);
-    if (packagesWithErrors.length === 0) {
-        return process.exit(0);
+    if (pckg.errors.deps.src.length) {
+        console.log(chalk.gray("[src] Source code"));
+        pckg.errors.deps.src.forEach((item, index) => {
+            console.log(`${index + 1}. ${item}`);
+        });
     }
 
-    console.log(chalk.red(`Source and dependencies listed in "package.json" file not synced:`));
-    packagesWithErrors.forEach((pckg, index) => {
-        console.log(chalk.red(`${index + 1}. ${pckg.packageJson.name} (${pckg.dir})`));
+    if (pckg.errors.deps.dependencies.length) {
+        console.log(chalk.gray("[package.json] dependencies:"));
+        pckg.errors.deps.dependencies.forEach((item, index) => {
+            console.log(`${index + 1}. ${item}`);
+        });
+    }
 
-        if (pckg.errors.deps.src.length) {
-            console.log(chalk.gray("[src] Source code"));
-            pckg.errors.deps.src.forEach((item, index) => {
-                console.log(`${index + 1}. ${item}`);
-            });
-        }
+    if (pckg.errors.deps.devDependencies.length) {
+        console.log(chalk.gray("[package.json] devDependencies:"));
+        pckg.errors.deps.devDependencies.forEach((item, index) => {
+            console.log(`${index + 1}. ${item}`);
+        });
+    }
 
-        if (pckg.errors.deps.dependencies.length) {
-            console.log(chalk.gray("[package.json] dependencies:"));
-            pckg.errors.deps.dependencies.forEach((item, index) => {
-                console.log(`${index + 1}. ${item}`);
-            });
-        }
+    if (pckg.errors.deps.peerDependencies.length) {
+        console.log(chalk.gray("[package.json] peerDependencies:"));
+        pckg.errors.deps.peerDependencies.forEach((item, index) => {
+            console.log(`${index + 1}. ${item}`);
+        });
+    }
 
-        if (pckg.errors.deps.devDependencies.length) {
-            console.log(chalk.gray("[package.json] devDependencies:"));
-            pckg.errors.deps.devDependencies.forEach((item, index) => {
-                console.log(`${index + 1}. ${item}`);
-            });
-        }
-
-        if (pckg.errors.deps.peerDependencies.length) {
-            console.log(chalk.gray("[package.json] peerDependencies:"));
-            pckg.errors.deps.peerDependencies.forEach((item, index) => {
-                console.log(`${index + 1}. ${item}`);
-            });
-        }
-
-        console.log();
-    });
-
-    process.exit(1);
+    console.log();
 });
-*/
+
+process.exit(1);
