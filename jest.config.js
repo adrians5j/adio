ac52b7541b9721faedb0a6abf184b79320b9d946
Pavel Denisjuk <pavel@webiny.com>
2025-02-18 15:20:49 +0100

    chore: simplify jest config

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,7 @@
-// Create a module map to point packages to the build output
-const moduleNameMapper = {};
-
-moduleNameMapper["^adio/(.*)$"] = "<rootDir>src/$1";
-moduleNameMapper["^adio$"] = "<rootDir>src";
-
 export default {
     rootDir: process.cwd(),
-    testMatch: ["**/*.test.js", "**/*.test.mjs"],
+    testMatch: ["**/*.test.js"],
     transform: {},
-    moduleNameMapper,
     coverageReporters: ["lcov", "html"],
     coveragePathIgnorePatterns: ["__tests__", "/node_modules/"],
     collectCoverageFrom: ["src/**/*.js"],
