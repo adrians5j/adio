99084d4145189039a1c68ee38ec161695f272cf0
Bruno Zorić <bruno.zoric@gmail.com>
2026-03-12 10:50:16 +0100

    feat: add missing chalk dependency

---
+++ .adiorc.js
@@ -0,0 +1,74 @@
+/**
+ * For self testing.
+ */
+import get from "lodash.get";
+
+export default {
+    parser: {
+        plugins: ["jsx", "classProperties", "dynamicImport", "throwExpressions", "typescript"]
+    },
+    traverse: ({ path, push }) => {
+        const { node } = path;
+        if (node.type === "CallExpression") {
+            if (
+                get(node, "callee.property.name") === "resolve" &&
+                get(node, "callee.object.name") === "require"
+            ) {
+                const possiblePackage = get(node, "arguments.0.value");
+                if (typeof possiblePackage === "string") {
+                    return push(possiblePackage);
+                }
+            }
+        }
+    },
+    ignore: {
+        src: [
+            "~tests",
+            "~",
+            "async_hooks",
+            "aws-sdk",
+            "buffer",
+            "child_process",
+            "crypto",
+            "events",
+            "follow-redirects",
+            "fs",
+            "http",
+            "https",
+            "module",
+            "inspector",
+            "node:fs",
+            "node:timers",
+            "node:path",
+            "node:stream",
+            "os",
+            "path",
+            "readline",
+            "stream",
+            "util",
+            "url",
+            "worker_threads"
+        ],
+        dependencies: [
+            "@babel/runtime",
+            // Packages below are defined as peerDependencies in many 3rd party packages
+            // and make yarn go crazy with warnings. We define these packages as "dependencies"
+            // in our own packages, but we don't directly use them:
+            "@emotion/react",
+            "@svgr/webpack",
+            "@types/react",
+            "@webiny/cli",
+            "prop-types",
+            "apollo-cache",
+            "apollo-client",
+            "apollo-link",
+            "apollo-utilities",
+            "graphql",
+            "react-dom"
+        ],
+        devDependencies: true,
+        peerDependencies: true
+    },
+    ignoreDirs: ["node_modules/", "dist/", "build/", "nextjs/"],
+    packages: ["./"]
+};
