5e2483520b14b4b8aaf9578a53f168c5264f0296
Bruno Zorić <bruno.zoric@gmail.com>
2026-03-17 14:11:05 +0100

    feat: deps

--- src/utils/testPackage.js
+++ src/utils/testPackage.js
@@ -1,15 +1,71 @@
 /**
  * Add node: prefix to all system packages.
  */
-const NODEJS_SYSTEM_PACKAGES = ["path", "os", "fs", "util", "events", "crypto", "url"].reduce(
-    (items, pkg) => {
-        items.push(pkg);
-        items.push(`node:${pkg}`);
+const NODEJS_SYSTEM_PACKAGES = [
+    "assert",
+    "assert/strict",
+    "async_hooks",
+    "buffer",
+    "child_process",
+    "cluster",
+    "console",
+    "constants",
+    "crypto",
+    "dgram",
+    "diagnostics_channel",
+    "dns",
+    "dns/promises",
+    "domain",
+    "events",
+    "fs",
+    "fs/promises",
+    "http",
+    "http2",
+    "https",
+    "inspector",
+    "inspector/promises",
+    "module",
+    "net",
+    "os",
+    "path",
+    "path/posix",
+    "path/win32",
+    "perf_hooks",
+    "process",
+    "punycode",
+    "querystring",
+    "readline",
+    "readline/promises",
+    "repl",
+    "sea",
+    "sqlite",
+    "stream",
+    "stream/consumers",
+    "stream/promises",
+    "stream/web",
+    "string_decoder",
+    "sys",
+    "test",
+    "test/reporters",
+    "timers",
+    "timers/promises",
+    "tls",
+    "trace_events",
+    "tty",
+    "url",
+    "util",
+    "util/types",
+    "v8",
+    "vm",
+    "wasi",
+    "worker_threads",
+    "zlib"
+].reduce((items, pkg) => {
+    items.push(pkg);
+    items.push(`node:${pkg}`);
 
-        return items;
-    },
-    []
-);
+    return items;
+}, []);
 
 export const extractDepsFromPackageJson = ({
     dependencies = {},
